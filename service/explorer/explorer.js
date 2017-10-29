9eefdf958b454b814a87c881acb5f101db77aa64
Ashlin <ashlin4010@users.noreply.github.com>
2017-10-30 11:43:44 +1300

    more or less working

--- service/explorer/explorer.js
+++ service/explorer/explorer.js
@@ -11,14 +11,17 @@ router.get("/"+url+"*", function(req, res) {
     let address = decodeURI(req.path).substring(url.length + 1);//removes url "+1" because we have a "/"
     if (address.endsWith("/")) address = address.slice(0, -1);//so we can compear them to see if we need to reload the page
     let contents = core.contents(address);
+    let addressO = address;
+    address = contents.address.replace(/\\/g, "/");
+    let addressArray = address.split("/");
+    if (addressArray[0] === "") addressArray.shift();
 
-    if(path.normalize(contents.address) !== path.normalize(address)){
-        res.redirect(`/${path.normalize(path.join(url,contents.address))}`.replace(/\\/g, "/")+"/");
+    if(path.normalize(contents.address) !== path.normalize(addressO)){
+        res.redirect(`/${path.normalize(path.join(url,address))}`.replace(/\\/g, "/")+"/");
         res.end();
         return
     }
-
-    res.send(core.contents(address));
+    res.render(path.join(__dirname,"index.ejs"),{files:contents.file,address:address,addressArray:addressArray,url:config.URLPrecursors, path:path});
 });
 
 
